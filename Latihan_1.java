6dd6f9245beea962476c4e7639ba274de9e20067
Aurelhijratulafifa <aurelhijratulafifa662@gmail.com>
2024-10-08 20:52:29 +0700

    Update Latihan_1.java

--- Latihan_1.java
+++ Latihan_1.java
@@ -1,22 +1,26 @@
 
-package Pratikum_1;
-import java.sql.*;
+package Test_Koneksi;
+
+import com.mysql.jdbc.Driver;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 
 public class Latihan_1 {
 public static void main(String[] args) {
 }
-    static final String db_unidha2="jdbc:mysql://localhost:3306/db_unidha2";
+    static final String db_url="jdbc:mysql://localhost:3306/db_unidha3";
     public static Connection db_unidha2() throws SQLException , ClassNotFoundException{
         try {
             Class.forName("com.mysql.jdbc.Driver");
             System.out.println("Proses Deteksi Driver Berhasil");
-            DriverManager.getConnection(db_unidha2,"root","");
+            DriverManager.getConnection(db_url,"root","");
             System.out.println("Koneksi Database Berhasil");
         }catch (final SQLException ex) {
             System.out.println("Koneksi Database Gagal");
         }
-        return null;
+        return Test_Konek;
         }
     
     }
